1822af0574ab5319c663d431876cf3fe848ebcce
Cheong Sik Feng <sikfeng@gmail.com>
2025-05-08 16:10:26 +0800

    remove unused imports

--- modules.py
+++ modules.py
@@ -1,9 +1,8 @@
 import math
-from typing import Dict, Optional, Tuple, Union
+from typing import Optional, Union
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from tensordict import TensorDict
 
 
